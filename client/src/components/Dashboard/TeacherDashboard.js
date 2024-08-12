346eedde524c4c58c368378a877e785068dc38fe
Saurabh Kashyap <125493327+Sau0908@users.noreply.github.com>
2024-08-12 21:37:19 +0530

    Update TeacherDashboard.js

--- client/src/components/Dashboard/TeacherDashboard.js
+++ client/src/components/Dashboard/TeacherDashboard.js
@@ -1,7 +1,14 @@
 import React from "react";
+import AssignStudent from "../Teacher/AssignStudent";
+import AssignClassroom from "../Teacher/AssignClassroom";
 
 const TeacherDashboard = () => {
-  return <div></div>;
+  return (
+    <div className="p-8">
+      <AssignStudent />
+      <AssignClassroom />
+    </div>
+  );
 };
 
 export default TeacherDashboard;
